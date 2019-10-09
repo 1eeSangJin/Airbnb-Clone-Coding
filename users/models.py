a05c6fd4bbc139402483cd39f229ed38401d3215
1eeSangJin <avesta_accel@naver.com>
2019-10-09 16:59:43 +0900

    #1.3 Create Apps and Setting User App

---
+++ users/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+
+    GENDER_CHOICES = ((GENDER_MALE, "male"), (GENDER_FEMALE, "female"))
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "ko"
+    LANGUAGE_JAPAN = "jp"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_JAPAN, "Japanese"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_JPY = "jpy"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_JPY, "JPY"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
