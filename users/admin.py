d3301c901cf8a1bb38cef282a86b67586211bdce
1eeSangJin <avesta_accel@naver.com>
2019-11-03 23:24:24 +0900

    #3.2 Github Login and Kakao Login, Tailwind CSS Setting

--- users/admin.py
+++ users/admin.py
@@ -19,12 +19,13 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
     )
 
-    list_filter = UserAdmin.list_filter + ("superhost",)
+    list_filter = UserAdmin.list_filter + ("superhost", "login_method")
 
     list_display = (
         "username",
@@ -39,4 +40,5 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "email_verified",
         "email_secret",
-    )    
+        "login_method",
+    )
